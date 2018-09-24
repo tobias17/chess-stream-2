140b355c872024c31f52c1fe0f4e0c8a7eec8a6c
Tobias Fischer <tobiasfischer17@gmail.com>
2018-09-24 13:19:44 -0400

    Hough Line Transform

--- app/src/main/java/com/example/tobias/chess_stream_2/MainActivity.java
+++ app/src/main/java/com/example/tobias/chess_stream_2/MainActivity.java
@@ -29,6 +29,9 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 // OpenCV Classes
@@ -131,12 +134,41 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         // TODO Auto-generated method stub
         mRgba = inputFrame.rgba();
 
-        boolean rotate = false;
+        boolean rotate = true;
         if (rotate) {
             // Rotate mRgba 90 degrees
             Core.transpose(mRgba, mRgbaT);
             Imgproc.resize(mRgbaT, mRgbaF, mRgbaF.size(), 0,0, 0);
             Core.flip(mRgbaF, mRgba, 1 );
+        }
+
+        Mat imgGray = new Mat();
+        Imgproc.cvtColor(mRgba, imgGray, Imgproc.COLOR_RGB2GRAY);
+
+        Mat imgThreshold = new Mat();
+        Imgproc.threshold(imgGray, imgThreshold, 127, 255, Imgproc.THRESH_BINARY + Imgproc.THRESH_OTSU);
+
+        Mat imgCanny = new Mat();
+        Imgproc.blur(imgGray, imgGray, new Size(3, 3));
+        Imgproc.Canny(imgGray, imgCanny, 10, 100, 3);
+
+        Mat lines = new Mat();
+        int threshold = 120;
+        int minLineSize = 40;
+        int lineGap = 20;
+
+        Imgproc.HoughLinesP(imgCanny, lines, 1, Math.PI / 180, threshold, minLineSize, lineGap);
+
+        for (int x = 0; x < lines.rows(); x++) {
+            double[] vec = lines.get(x,0);
+            double x1 = vec[0],
+                    y1 = vec[1],
+                    x2 = vec[2],
+                    y2 = vec[3];
+            Point start = new Point(x1, y1);
+            Point end = new Point(x2, y2);
+
+            Imgproc.line(mRgba, start, end, new Scalar(255, 0, 0), 3);
         }
 
         return mRgba; // This function must return
